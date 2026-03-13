fce6c0a2d6ebd34390055c730a3ea2d48d245950
Btema2 <temabiill@gmail.com>
2026-03-13 09:27:31 +0100

    rm: remove array of heroes, as it has no purpuse

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 import Hero.Hero;
 import Window.MyWindow;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Hero> Heroes = new ArrayList<>();
         Hero bulbasaur = new Hero(10, 10, 100, 10);
-        Heroes.add(bulbasaur);
         new MyWindow(600, 600);
     }
 }
